044b16ff4dff8e79d8baacf1b89d337168f3427d
Tanish Mahajan <mahajantanish6@gmail.com>
2026-07-11 22:49:48 +0530

    Time: 1 ms (17.67%) | Memory: 23.9 MB (19.23%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i =0;
+        int n = nums.size();
+        int j =0;
+        while(j<n){
+            if(nums[j]!=0){
+                swap(nums[i++],nums[j++]);
+            }
+            else{
+                j++;
+            }
+        }
+    }
+};
