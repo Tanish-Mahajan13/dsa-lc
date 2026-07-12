fdff9bee1fc9989705be65aa020d100bc7079a84
Tanish Mahajan <mahajantanish6@gmail.com>
2026-07-12 23:08:06 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (69.38%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,9 +3,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k=k%n;
-        std::reverse(nums.begin(),nums.end());
-        std::reverse(nums.begin(),nums.begin()+k);
-        std::reverse(nums.begin()+k,nums.end());
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
 
     }
 };
